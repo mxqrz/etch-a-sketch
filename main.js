220accca9d13c59290fcb14e9d726e0f2ad3a8cd
grimflame <iconcosmicalpha@gmail.com>
2022-05-21 02:38:13 -0400

    Grid size scroll updates real-time

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+const grid = document.querySelector(".grid");
+const grid_size = document.querySelector("#grid-size")
+const color_selector = document.querySelector("#color");
+const rainbow_toggle = document.querySelector("#rainbow");
+
 let genGrid = (size) =>
 {
     let n = Math.pow(size, 2);
+
+    document.querySelectorAll(".item").forEach(item =>
+    {
+        item.remove();
+    });
+
     for (let i = 0; i < n; i++)
     {
         let gridItem = document.createElement("div");
@@ -14,10 +25,12 @@ let genGrid = (size) =>
             {
                 colX += "auto ";
             }
-            document.querySelector(".grid").style.gridTemplateColumns = colX;
+            grid.style.gridTemplateColumns = colX;
         }
-        console.log('hi');
     }
 }
 
-genGrid(5);
+grid_size.addEventListener('input', function ()
+{
+    genGrid(grid_size.value);
+});
